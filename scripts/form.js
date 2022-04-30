ac646d9ae2bc19c03e8ab82b5636154c59084864
KarolinaCiuk <ciuk.karolina@gmail.com>
2022-04-30 17:28:00 +0200

    all project's files

---
+++ scripts/form.js
@@ -0,0 +1,48 @@
+// Creation variable today in yyyy-mm-dd format
+let today = new Date();
+let dd = today.getDate();
+let mm = today.getMonth() + 1; //January is 0
+let yyyy = today.getFullYear();
+
+if (dd < 10) {
+  dd = "0" + dd;
+}
+if (mm < 10) {
+  mm = "0" + mm;
+}
+
+today = yyyy + "-" + mm + "-" + dd;
+document.getElementById("input_date").setAttribute("min", today);
+
+// Creation variable maxday in yyyy-mm-dd format
+const days = 13;
+// Date.now() gives the epoch date value (in milliseconds) of current date
+maxDate = new Date(Date.now() + days * 24 * 60 * 60 * 1000);
+
+let ddMax = maxDate.getDate();
+let mmMax = maxDate.getMonth() + 1;
+let yyyyMax = maxDate.getFullYear();
+
+if (ddMax < 10) {
+  ddMax = "0" + ddMax;
+}
+if (mmMax < 10) {
+  mmMax = "0" + mmMax;
+}
+
+maxDate = yyyyMax + "-" + mmMax + "-" + ddMax;
+document.getElementById("input_date").setAttribute("max", maxDate);
+
+const $name = document.getElementById("name_input");
+const $backButton = document.getElementById("back_btn");
+const $buyButton = document.getElementById("buy_btn");
+
+$backButton.addEventListener("click", () => {
+  window.location.href = "home_page.html";
+});
+
+$buyButton.addEventListener("click", () => {
+  const name = $name.value;
+  console.log(name);
+  window.location.href = "thanks.html";
+});
---
+++ scripts/thanks.js
@@ -0,0 +1,5 @@
+const backHomePage = document.getElementById("back_hp")
+
+backHomePage.addEventListener("click", () => {
+    window.location.href = "home_page.html";
+});
